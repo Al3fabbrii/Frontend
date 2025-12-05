331283f3c138f2180ab397bff1cd10c3df6322a0
Ale <alessandr04.fabbri@edu.unife.it>
2025-12-05 14:04:51 +0100

    Added mock API

--- src/app/core/models/product.ts
+++ src/app/core/models/product.ts
@@ -1,7 +1,10 @@
 export interface Product {
     id: string;
     title: string;
+    description: string;
     price: number;
+    originalPrice: number;
+    sale: boolean;
     thumbnail?: string;
     tags?: string[];
     createdAt: string;
--- src/app/core/services/product-api.ts
+++ src/app/core/services/product-api.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
-import { PRODUCTS_MOCK } from '../mocks/product.mock';
 import { delay, Observable, of } from 'rxjs';
 import { Product } from '../models/product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductApi {
+  private readonly baseUrl = 'http://localhost:3000/api';
+
+  constructor(private readonly http: HttpClient) { }
   list(): Observable<Product[]> {
-    return of(PRODUCTS_MOCK).pipe(delay(300));
+    return this.http.get<Product[]>(`${this.baseUrl}/products`);
   }
 }
